3be4ae81a303abff54d3771ec3ed7fffffc95804
stla <laurent_step@yahoo.fr>
2022-07-20 16:13:31 +0200

    renaming

--- src/Utils.hs
+++ src/Utils.hs
@@ -9,8 +9,8 @@ import           Data.Sequence as DS (Seq, fromList, (><), filter)
 triangleEdges :: Triangle -> Seq Edge
 triangleEdges (Triangle i j k) = fromList [Edge i j, Edge j k, Edge i k]
 
-isBorderEdge :: Eq a => Seq a -> a -> Bool
-isBorderEdge xs x = length (DS.filter (== x) xs) == 1
+isUnique :: Eq a => Seq a -> a -> Bool
+isUnique xs x = length (DS.filter (== x) xs) == 1
 
 allEdges :: Triangulation -> Seq Edge
 allEdges triangulation = 
@@ -18,7 +18,7 @@ allEdges triangulation =
     foldl1 (><) triplets
 
 borderEdges :: Triangulation -> Seq Edge
-borderEdges triangulation = DS.filter (isBorderEdge edges) edges
+borderEdges triangulation = DS.filter (isUnique edges) edges
   where
     edges = allEdges triangulation
 
