aa287cc5a00357dd051ec2c682b68e98c004c157
stla <laurent_step@yahoo.fr>
2022-07-20 17:53:33 +0200

    borderEdges2

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,10 +1,14 @@
 module Utils
   ( borderEdges
+  , borderEdges2
   )
 where
     
 import           Types 
-import           Data.Sequence as DS (Seq, fromList, (><), filter) 
+import           Data.Sequence as DS     (Seq, fromList, (><), filter, drop) 
+import           Data.Maybe              (isNothing, fromJust)
+import           Data.Foldable           (find)
+import           Data.Foldable.WithIndex (ifind)
 
 triangleEdges :: Triangle -> Seq Edge
 triangleEdges (Triangle i j k) = fromList [Edge i j, Edge j k, Edge i k]
@@ -22,3 +26,14 @@ borderEdges triangulation = DS.filter (isUnique edges) edges
   where
     edges = allEdges triangulation
 
+isUnique2 :: Eq a => Seq a -> a -> Bool
+isUnique2 xs x = isNothing y 
+  where
+    (i, _) = fromJust $ ifind (\_ x' -> x' == x) xs
+    y = find (== x) (DS.drop i xs)
+
+
+borderEdges2 :: Triangulation -> Seq Edge
+borderEdges2 triangulation = DS.filter (isUnique2 edges) edges
+  where
+    edges = allEdges triangulation
